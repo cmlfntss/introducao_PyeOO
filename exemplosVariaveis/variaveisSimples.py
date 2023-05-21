51f937a492559e653a97060c9ca8a79737730f97
cmlfntss <camiladejesusfontes@gmail.com>
2023-05-21 20:42:19 -0300

    exemplos de nomes de variáveis simples em python

---
+++ exemplosVariaveis/variaveisSimples.py
@@ -0,0 +1,18 @@
+# Variáveis: definições e tipos
+
+# Declarando 4 variáveis e atribuindo os valores a elas
+
+uma_string = "Aluno"
+um_inteiro = 7
+um_float = 7.939
+um_booleano = False
+
+# Imprimindo o valor e o tipo de uma das variáveis
+
+print(uma_string)
+print(type(um_booleano))
+
+# Casting: convertendo a variável do tipo float para int e para string
+
+print(int(um_float))
+print(str(um_float))
