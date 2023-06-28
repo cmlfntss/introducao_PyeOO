59beada8720894ac93cae20f0679883afa175850
cmlfntss <camiladejesusfontes@gmail.com>
2023-06-28 19:10:09 -0300

    subindo cód exemplo pessoas

---
+++ exemploPessoas/exemploPessoas_2.py
@@ -0,0 +1,11 @@
+class Pessoa:
+    num_pessoas = 0
+    def __init__(self, nome):
+        self.nome = nome
+        Pessoa.num_pessoas += 1
+
+
+nomes = ['João', 'Paulo', 'Jorge', 'Camila']
+pessoas = [Pessoa(nome) for nome in nomes]
+print(Pessoa.num_pessoas)
+print(pessoas[0].nome)
